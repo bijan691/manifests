f45cf9d2f9e8ae1bd2ca5e15434fac1f2c07f96f
bijan691 <bijan691@gmail.com>
2017-02-08 01:26:55 -0600

    Update site.pp

--- site.pp
+++ site.pp
@@ -8,4 +8,18 @@ mode => '0666',
 
 package{"vsftpd": ensure => present,}
 service {"vsftpd": ensure => running,}
+
+user{'eric':
+ensure => present,
+comment => 'Eric, Good',
+home => '/home/eric',
+managehome => true,
+ }
+ user{'ram':
+ensure => present,
+comment => 'Ram, gopal',
+home => '/home/ram',
+managehome => true,
+ }
+ 
 }
