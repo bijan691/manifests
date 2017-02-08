ac6f1ac91c5ca6a6555fd1297c11fefc84d38620
bijan691 <bijan691@gmail.com>
2017-02-08 01:08:33 -0600

    sdf
    dsf

---
+++ site.pp
@@ -0,0 +1,18 @@
+node default {
+file { '/tmp/hello':
+content => "Hello, World\n",
+owner => 'root',
+group => 'root',
+mode => '0666',
+}
+
+package{"vsftpd": ensure => present,}
+service {"vsftpd": ensure => running,}
+
+user{'eric':
+ensure => present,
+comment => 'Eric, Good',
+home => '/home/eric',
+managehome => true,
+ }
+}
